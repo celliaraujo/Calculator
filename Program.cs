985f04cfc0d73822c055c39444f63009f602391e
celliaraujo <celliaraujo@gmail.com>
2024-02-17 14:04:07 -0300

    first-commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Informe primeiro valor:");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Informe segundo valor:");
+        float v2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{v1} + {v2} é igual a {v1+v2}.");
+    }
+}
